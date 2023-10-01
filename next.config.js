5b7f6d745455c65a2e0240b96788bf397c95ff4a
ffaheem <faisal.bin.faheem@gmail.com>
2023-10-01 13:39:16 +0500

    next cinfig changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    serverMinification: false,
+  },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
